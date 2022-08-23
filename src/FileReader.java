df5ef86bb8355ce649e927fecfe22b4ad2044809
Marvin Mino <marvinmino1@gmail.com>
2022-08-23 22:27:02 +0200

    fixed the exception handling and change the logic for the number of cities and names

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,39 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class FileReader {
     String _filePath;
-    
-    public FileReader(String filePath){
+    ArrayList<User> _users;
+
+    public FileReader(String filePath) throws FileNotFoundException {
         _filePath = filePath;
+        _users = read();
     }
 
     public ArrayList<User> read() throws FileNotFoundException {
         ArrayList<User> users = new ArrayList<User>();
 
-        try {
-            File myObj = new File(_filePath);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-
-                users.add(new User(data.split(",")));
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+        File file = new File(_filePath);
+        if(!file.exists()){
+            throw new FileNotFoundException("File not found");
         }
+        Scanner myReader = new Scanner(file);
+
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            users.add(new User(data.split(",")));
+        }
+
+        myReader.close();
         return users;
     }
 
-    public int getNumberOfCities(ArrayList<User> users){
+    public int getNumberOfCities(){
         int count = 0;
         int maxCount = 0;
-        User[] usersArray = users.toArray(new User[0]);
+        User[] usersArray = _users.toArray(new User[0]);
         for(int i = 0; i < usersArray.length; i++){
             for(int j = 0; j < usersArray.length; j++){
                 if(usersArray[i].get_city().equals(usersArray[j].get_city())){
@@ -48,10 +48,10 @@ class FileReader {
         return maxCount;
     }
 
-    public int getNumberOfNames(ArrayList<User> users){
+    public int getNumberOfNames(){
         int count = 0;
         int maxCount = 0;
-        User[] usersArray = users.toArray(new User[0]);
+        User[] usersArray = _users.toArray(new User[0]);
         for(int i = 0; i < usersArray.length; i++){
             for(int j = 0; j < usersArray.length; j++){
                 if(usersArray[i].get_name().equals(usersArray[j].get_name())){
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args){
-        FileReader reader = new FileReader("C:\\Users\\marvi\\IdeaProjects\\FileReader\\src\\file.txt");
         try{
-            ArrayList<User> users = reader.read();
-            System.out.println("The number of distinct cities is: " + reader.getNumberOfCities(users));
-            System.out.println("The number of distinct names is: " + reader.getNumberOfNames(users));
+            FileReader reader = new FileReader("C:\\Users\\marvi\\IdeaProjects\\FileReader\\src\\file.txt");
+            System.out.println("The number of distinct cities is: " + reader.getNumberOfCities());
+            System.out.println("The number of distinct names is: " + reader.getNumberOfNames());
+        } catch (FileNotFoundException e){
+            System.out.printf("File not found");
         } catch (Exception e){
-            e.printStackTrace();
             System.out.printf("There was an error with reading the file");
         }
     }
